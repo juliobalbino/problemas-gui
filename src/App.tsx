b1ed31ee44a363c6c4e06ff289473e05db40858b
Julio Balbino <julio4110@hotmail.com>
2021-10-29 22:32:48 -0300

    ta ai

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/statics/navbar/Navbar';
+import Sobre from './pages/sobre/Sobre';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
